96242a6546674512a4534c256c19a5feed5b61e0
Antonio <116432648+AntonioBG89@users.noreply.github.com>
2024-07-03 20:22:36 +0000

    Casi esta

--- src/app.js
+++ src/app.js
@@ -4,8 +4,61 @@ import "./style.css";
 
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
+/*
+ */
+
+function generandoPosicionesAleatorias(a) {
+  return Math.floor(Math.random() * a);
+}
+
+let insertarDatosNumericos = document.getElementById("numero");
+let insertarDatosPalo1 = document.getElementById("palo1");
+let insertarDatosPalo2 = document.getElementById("palo2");
+
+let arrayPalos = ["♦", "♥", "♠", "♣"];
+let arrayNumeros = [
+  "1",
+  "2",
+  "3",
+  "4",
+  "5",
+  "6",
+  "7",
+  "8",
+  "9",
+  "10",
+  "11",
+  "12"
+];
+
+function randomCardGenerator() {
+  function generandoAleatorizacion(b) {
+    let posicionAleatoria = generandoPosicionesAleatorias(b);
+    return posicionAleatoria;
+  }
+  function insertarDatos() {
+    insertarDatosPalo1.innerHTML = arrayPalos[generandoAleatorizacion(4)];
+    insertarDatosPalo2.innerHTML = insertarDatosPalo1.innerHTML;
+    insertarDatosNumericos.innerHTML =
+      arrayNumeros[generandoAleatorizacion(12)];
+  }
+}
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+
+  setInterval(myTimer, 1000);
+  function myTimer() {
+    const d = new Date();
+    document.getElementById("demo").innerHTML = d.toLocaleTimeString();
+  }
+
+  if (
+    insertarDatosPalo1.innerHTML == "♦" ||
+    insertarDatosPalo2.innerHTML == "♥"
+  ) {
+    document.getElementById("palo1").style.color = "red";
+    document.getElementById("numero").style.color = "red";
+    document.getElementById("palo2").style.color = "red";
+  }
 };
